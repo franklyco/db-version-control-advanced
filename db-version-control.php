42bba269b37aa8a37e1ff21ff2d65f628f0a0414
franklyco <79901287+franklyco@users.noreply.github.com>
2025-09-09 20:17:41 -0400

    Update for 1.1.5

--- db-version-control.php
+++ db-version-control.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: DB Version Control Advanced
  * Description: Sync WordPress to version-controlled JSON files for easy Git workflows. A fork of DB Version Control Main
- * Version:     1.1.4
+ * Version:     1.1.5
  * Author:      Frankly / Robert DeVore
  * Author URI:  https://frankly.design
  * Text Domain: dbvc
