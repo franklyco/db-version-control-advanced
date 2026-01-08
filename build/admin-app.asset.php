ee3f2315330c360a201029e926e6eb4eb6969950
franklyco <79901287+franklyco@users.noreply.github.com>
2026-01-08 00:21:41 -0500

    1.3.4

--- build/admin-app.asset.php
+++ build/admin-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-element'), 'version' => 'b87452efaf2cdcece2db');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-element'), 'version' => 'aced7aed97c5684cd547');
